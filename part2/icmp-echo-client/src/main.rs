3b6d10e29cd64455b15c697f30ebfac40a6ab519
Soha Jin <soha@lohu.info>
2022-01-28 22:34:52 +0800

    just append for non-BSD user

--- part2/icmp-echo-client/src/main.rs
+++ part2/icmp-echo-client/src/main.rs
@@ -8,15 +8,11 @@ use pnet::{
 		ip::IpNextHeaderProtocols::Icmpv6
 	}
 };
-use std::cmp::max;
 
 const CODE_LEN: usize = 4;
-const CODE: [u8; CODE_LEN] = [b' ', b'D', b'1', b'6'];
+const CODE: [u8; CODE_LEN] = [b'm', b'D', b'1', b'6'];
 const ICMP_HEADER_LEN: usize = 4;
 const ECHO_HEADER_LEN: usize = 4;
-const CODE_WITH_ECHO_HEADER_LEN: usize = ECHO_HEADER_LEN + CODE_LEN;
-const BSD_PING_WITH_TIMESTAMP_LEN: usize = ECHO_HEADER_LEN + 8;
-const GNU_PING_WITH_TIMESTAMP_LEN: usize = ECHO_HEADER_LEN + 16;
 
 fn main() {
 	let (mut tx, mut rx) = match transport_channel(4096, TransportChannelType::Layer4(TransportProtocol::Ipv6(Icmpv6))) {
@@ -36,23 +32,9 @@ fn main() {
 				println!("Received an ICMP echo request from {} with ID and Sequence# {:02x?}", addr, &payload[..ECHO_HEADER_LEN]);
 
 				let payload_len = payload.len();
-				let mut buf_len = ECHO_HEADER_LEN + payload_len;
+				let buf_len = ECHO_HEADER_LEN + payload_len + CODE_LEN;
 				buf[ICMP_HEADER_LEN..(ICMP_HEADER_LEN + payload_len)].copy_from_slice(&payload[..payload_len]);
-				match payload_len {
-					0..CODE_WITH_ECHO_HEADER_LEN | BSD_PING_WITH_TIMESTAMP_LEN | GNU_PING_WITH_TIMESTAMP_LEN => {
-						buf_len += CODE_LEN;
-						buf[(ICMP_HEADER_LEN + payload_len)..buf_len].copy_from_slice(&CODE);
-					},
-					_ => {
-						let offset = match payload_len {
-							0..=BSD_PING_WITH_TIMESTAMP_LEN => ECHO_HEADER_LEN,
-							0..=GNU_PING_WITH_TIMESTAMP_LEN => BSD_PING_WITH_TIMESTAMP_LEN,
-							_ => GNU_PING_WITH_TIMESTAMP_LEN
-						};
-						buf_len = max(buf_len, ICMP_HEADER_LEN + offset + CODE_LEN);
-						buf[(ICMP_HEADER_LEN + offset)..(ICMP_HEADER_LEN + offset + CODE_LEN)].copy_from_slice(&CODE);
-					}
-				};
+				buf[(ICMP_HEADER_LEN + payload_len)..buf_len].copy_from_slice(&CODE);
 				
 				let new = Icmpv6Packet::new(&buf[..buf_len]).unwrap();
 				match tx.send_to(new, addr) {
