ebd8a4b4b95b7d6d196693e7faada4a85ef32149
Soha Jin <soha@lohu.info>
2022-01-28 22:24:26 +0800

    ping

---
+++ part2/icmp-echo-client/README.md
@@ -0,0 +1,4 @@
+# Soha's Red Packet for 2022
+## Ping
+
+Don't forget `sysctl net.ipv6.icmp.echo_ignore_all=1`.
---
+++ part2/icmp-echo-client/src/main.rs
@@ -0,0 +1,66 @@
+#![feature(exclusive_range_pattern)]
+
+use pnet::{
+	transport::{TransportChannelType, TransportProtocol, transport_channel, icmpv6_packet_iter},
+	packet::{
+		Packet, 
+		icmpv6::{Icmpv6Types::{EchoRequest, EchoReply}, Icmpv6Packet},
+		ip::IpNextHeaderProtocols::Icmpv6
+	}
+};
+use std::cmp::max;
+
+const CODE_LEN: usize = 4;
+const CODE: [u8; CODE_LEN] = [b' ', b'D', b'1', b'6'];
+const ICMP_HEADER_LEN: usize = 4;
+const ECHO_HEADER_LEN: usize = 4;
+const CODE_WITH_ECHO_HEADER_LEN: usize = ECHO_HEADER_LEN + CODE_LEN;
+const BSD_PING_WITH_TIMESTAMP_LEN: usize = ECHO_HEADER_LEN + 8;
+const GNU_PING_WITH_TIMESTAMP_LEN: usize = ECHO_HEADER_LEN + 16;
+
+fn main() {
+	let (mut tx, mut rx) = match transport_channel(4096, TransportChannelType::Layer4(TransportProtocol::Ipv6(Icmpv6))) {
+		Ok((tx, rx)) => (tx, rx),
+		Err(e) => panic!("Failed to create the channel: {}", e)
+	};
+	let mut rx_iter = icmpv6_packet_iter(&mut rx);
+	let mut buf = [0u8; 65536];
+	buf[0] = EchoReply.0;
+	println!("Hongbao is ready.");
+	loop {
+		match rx_iter.next() {
+			Ok((packet, addr)) => {
+				if packet.get_icmpv6_type() != EchoRequest { continue; }
+				let payload = packet.payload();
+				if payload.len() < ECHO_HEADER_LEN { continue; }
+				println!("Received an ICMP echo request from {} with ID and Sequence# {:02x?}", addr, &payload[..ECHO_HEADER_LEN]);
+
+				let payload_len = payload.len();
+				let mut buf_len = ECHO_HEADER_LEN + payload_len;
+				buf[ICMP_HEADER_LEN..(ICMP_HEADER_LEN + payload_len)].copy_from_slice(&payload[..payload_len]);
+				match payload_len {
+					0..CODE_WITH_ECHO_HEADER_LEN | BSD_PING_WITH_TIMESTAMP_LEN | GNU_PING_WITH_TIMESTAMP_LEN => {
+						buf_len += CODE_LEN;
+						buf[(ICMP_HEADER_LEN + payload_len)..buf_len].copy_from_slice(&CODE);
+					},
+					_ => {
+						let offset = match payload_len {
+							0..=BSD_PING_WITH_TIMESTAMP_LEN => ECHO_HEADER_LEN,
+							0..=GNU_PING_WITH_TIMESTAMP_LEN => BSD_PING_WITH_TIMESTAMP_LEN,
+							_ => GNU_PING_WITH_TIMESTAMP_LEN
+						};
+						buf_len = max(buf_len, ICMP_HEADER_LEN + offset + CODE_LEN);
+						buf[(ICMP_HEADER_LEN + offset)..(ICMP_HEADER_LEN + offset + CODE_LEN)].copy_from_slice(&CODE);
+					}
+				};
+				
+				let new = Icmpv6Packet::new(&buf[..buf_len]).unwrap();
+				match tx.send_to(new, addr) {
+					Ok(n) => println!("Sent an ICMP echo reply with {} bytes payload to {}", n, addr),
+					Err(e) => eprintln!("Failed to send reply to {}: {}", addr, e)
+				}
+			},
+			Err(e) => panic!("Failed to receive a packet: {}", e)
+		}
+	}
+}
